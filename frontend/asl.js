853fc11dd616f4988d73dd98256778f87e4532f9
Mezwer <95189950+Mezwer@users.noreply.github.com>
2024-11-02 19:41:11 -0500

    frontend asl page functioning

---
+++ frontend/asl.js
@@ -0,0 +1,26 @@
+const video = document.getElementById("webcam");
+const introClick = document.getElementById("finish");
+const intro = false;
+
+async function startWebcam() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    document.getElementById("progress").removeAttribute("hidden");
+  } catch (error) {
+    console.error("Error accessing the webcam:", error);
+    alert("Unable to access the webcam. Please check your permissions.");
+  }
+}
+
+// call when intro becomes true
+function onIntroFinish() {
+  const body = document.getElementsByClassName("main-content")[0];
+  introClick.removeAttribute("hidden");
+  body.style.marginTop = "-10%";
+}
+
+startWebcam();
+setTimeout(() => {
+  onIntroFinish();
+} , 1000); // temporary, change later
